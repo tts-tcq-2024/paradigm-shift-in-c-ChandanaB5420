66d7fef491983ac715408252d7f5bd996695684c
ChandanaB5420 <chandanac653@gmail.com>
2024-10-16 15:54:59 +0530

    Update warning_system.c

--- warning_system.c
+++ warning_system.c
@@ -3,20 +3,23 @@
 
 float tolerance = 0.05;  // 5% tolerance
 
-void printWarningMessage(const char* message) {
+void printWarningMessage(const char* message) 
+{
     if (message[0] != '\0') {
         printf("%s", message);
     }
 }
 
-void checkLowThreshold(float value, float min, float max, const char* lowWarningMessage) {
+void checkLowThreshold(float value, float min, float max, const char* lowWarningMessage) 
+{
     float lowerWarningLimit = min + (tolerance * max);
     if (value > min && value <= lowerWarningLimit) {
         printWarningMessage(lowWarningMessage);
     }
 }
 
-void checkHighThreshold(float value, float max, const char* highWarningMessage) {
+void checkHighThreshold(float value, float max, const char* highWarningMessage) 
+{
     float upperWarningLimit = max - (tolerance * max);
     if (value >= upperWarningLimit && value < max) {
         printWarningMessage(highWarningMessage);
