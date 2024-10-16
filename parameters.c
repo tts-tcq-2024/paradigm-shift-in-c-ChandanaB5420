d5880033c2c20ebfae20e2b20efd931cbd527938
ChandanaB5420 <chandanac653@gmail.com>
2024-10-16 15:51:59 +0530

    Update parameters.c

--- parameters.c
+++ parameters.c
@@ -2,7 +2,8 @@
 #include "parameter_checks.h"
 #include "warning_system.h"
 
-int checkParameter(float value, float min, float max, const char* errorMessage, const char* lowWarning, const char* highWarning) {
+int checkParameter(float value, float min, float max, const char* errorMessage, const char* lowWarning, const char* highWarning) 
+{
     if (value < min || value > max) {
         printf("%s", errorMessage);
         return 0;
