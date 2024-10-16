340db06da551bb2a0e5d4120a806faa2568886e4
ChandanaB5420 <chandanac653@gmail.com>
2024-10-16 15:49:04 +0530

    Update warning_system.h

--- warning_system.h
+++ warning_system.h
@@ -1,1 +1,5 @@
+extern float tolerance;
 
+void printWarningMessage(const char* message);
+void checkLowThreshold(float value, float min, float max, const char* lowWarningMessage);
+void checkHighThreshold(float value, float max, const char* highWarningMessage);
