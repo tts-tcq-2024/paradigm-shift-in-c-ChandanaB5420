cb9cfc1682fecc7fc054431c04c6315100760512
ChandanaB5420 <chandanac653@gmail.com>
2024-10-16 15:50:30 +0530

    Update parameters.c

--- parameters.c
+++ parameters.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include "parameter_checks.h"
+#include "warning_system.h"
 
+int checkParameter(float value, float min, float max, const char* errorMessage, const char* lowWarning, const char* highWarning) {
+    if (value < min || value > max) {
+        printf("%s", errorMessage);
+        return 0;
+    }
+
+    checkLowThreshold(value, min, max, lowWarning);
+    checkHighThreshold(value, max, highWarning);
+
+    return 1;
+}
